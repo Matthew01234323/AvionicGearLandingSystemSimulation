ec3e6308d798e626132f84545837041ef866c0c0
Matthew01234323 <30250523@blackpool.ac.uk>
2026-01-21 15:00:08 +0000

    Added HydraulicActuator Class which retains an extended value Added extending as a method of HydraulicActuators command_gear_down now calls HydraulicActuator.extending

--- simulation.py
+++ simulation.py
@@ -9,6 +9,27 @@ class GearState(Enum):
     DOWN_LOCKED = auto()
     TRANSITIONING_DOWN = auto()
 
+class HydraulicActuators:
+    temperature = 30
+    angle = 0
+    fault = False
+    
+    def extending(self, timePeriod):
+        if self.fault == False:
+            efficiency = 0.1 #how much angle changes every ms, add wind temperature, damage
+            temp = timePeriod
+            for t in range (0,temp):
+                time.sleep(0.01)
+                timePeriod -= 1
+                self.angle += efficiency
+                if self.angle >= 90: #stop extending at 90 degrees
+                    self.angle = 90
+                    break
+            return self.angle, timePeriod #returns the extended angle and time
+        
+        else:
+            self.log("Command Rejected")
+
 class LandingGearController:
     def __init__(self):
         self.state = GearState.UP_LOCKED
@@ -19,9 +40,16 @@ class LandingGearController:
     def command_gear_down(self):
         if self.state == GearState.UP_LOCKED:
             self.state = GearState.TRANSITIONING_DOWN
+            #HydraulicActuators.extending() ~~Add in functionality to ensure hydraulics fully exteneded
             self.log("Gear Deploying")
-            time.sleep(1)
-            self.log("Gear Deployed")
+            #time.sleep(1)
+            HydraulicsAngle, timePeriod = HydraulicActuators.extending(Hydraulics, 1200)
+            #print (HydraulicsAngle)
+            if HydraulicsAngle == 90:
+                self.state = GearState.DOWN_LOCKED
+                self.log("Gear Deployed")
+            else:
+                self.log("Insufficient Gear Deployment angle")
         else:
             self.log("Command Rejected, Invalid State")
         
@@ -35,6 +63,9 @@ class LandingGearController:
         else:
             self.log("Command Rejected, Invalid State")
 
+Hydraulics = HydraulicActuators
 controller = LandingGearController()
 controller.command_gear_down()
 controller.command_gear_up()
+#angle, time = HydraulicActuators.extending(Hydraulics, 1200)
+#print(time)
