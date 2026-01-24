0b86866403091dd4c1de5a93ee975cf9d4d84ef4
Matthew01234323 <30250523@blackpool.ac.uk>
2026-01-24 12:29:35 +0000

    Added retracting method within the HydraulicActuator Class Improved commenting Added hydraulicDebug feedback

--- simulation.py
+++ simulation.py
@@ -2,6 +2,11 @@ from enum import Enum, auto
 from random import randint
 import time
 
+def hydraulicDebug(self, timePeriod, debug):
+    if debug == True:
+        print ("time left:", timePeriod)
+        print ("efficiency:", self.efficiency)
+
 class GearState(Enum):
     UP_LOCKED = auto()
     TRANSITIONING_UP = auto()
@@ -9,13 +14,16 @@ class GearState(Enum):
     DOWN_LOCKED = auto()
     TRANSITIONING_DOWN = auto()
 
+    STATIONARY_LOCKED = auto()
+    STATIONARY = auto()
+
 class HydraulicActuators:
     temperature = 30
     angle = 0
     fault = False
     efficiency = 0.12
 
-    def refreshEfficiency(self, windMPH, altitudeFT):
+    def refreshEfficiency(self, windMPH, altitudeFT):       # Calculates the efficiency of the hydraulics
         self.efficiency = (0.10 - (windMPH * 0.00007)) - (altitudeFT * 0.000005)
         if self.temperature < -20:
             self.efficiency *= 0.8
@@ -24,17 +32,33 @@ class HydraulicActuators:
             self.efficiency *= 0.9
     
     def extending(self, timePeriod):
-        print(self.efficiency)
         if self.fault == False:
             temp = timePeriod
             for t in range (0,temp):
-                time.sleep(0.01)
+                time.sleep(0.01)                            # causes a real time simulation delay
                 timePeriod -= 1
                 self.angle += self.efficiency
-                if self.angle >= 90: #stop extending at 90 degrees
+                if self.angle >= 90:                        # stops extending at 90 degrees
                     self.angle = 90
                     break
-            return self.angle, timePeriod #returns the extended angle and time
+            hydraulicDebug (self, timePeriod, debug)
+            return self.angle, timePeriod                   # returns the extended angle and time
+        
+        else:
+            self.log("Command Rejected")
+
+    def retracting(self, timePeriod):
+        if self.fault == False:
+            temp = timePeriod
+            for t in range (0,temp):
+                time.sleep(0.01)                            # causes a real time simulation delay
+                timePeriod -= 1
+                self.angle -= self.efficiency
+                if self.angle <= 0:                        # stops extending at 90 degrees
+                    self.angle = 0
+                    break
+            hydraulicDebug (self, timePeriod, debug)
+            return self.angle, timePeriod                   # returns the extended angle and time
         
         else:
             self.log("Command Rejected")
@@ -47,37 +71,36 @@ class LandingGearController:
         print(f"[{self.state.name}] {message}")
 
     def command_gear_down(self):
-        if self.state == GearState.UP_LOCKED:
+        if self.state == GearState.UP_LOCKED:                       # Confirms the gear is in a valid state
             self.state = GearState.TRANSITIONING_DOWN
-            #HydraulicActuators.extending() ~~Add in functionality to ensure hydraulics fully exteneded
             self.log("Gear Deploying")
-            #time.sleep(1)
-            HydraulicsAngle, timePeriod = HydraulicActuators.extending(Hydraulics, 1200)
-            print (HydraulicsAngle)
-            print (timePeriod)
-            if HydraulicsAngle == 90:
+            HydraulicsAngle, timePeriod = HydraulicActuators.extending(Hydraulics, 1200) # Calls the hydraulics to move
+            if HydraulicsAngle == 90:                               # If the gear does extend enough
                 self.state = GearState.DOWN_LOCKED
                 self.log("Gear Deployed")
-            else:
+            else:                                                   # If the gear does not extend enough
                 self.log("Insufficient Gear Deployment angle")
         else:
-            self.log("Command Rejected, Invalid State")
+            self.log("Command Rejected")
         
     def command_gear_up(self):
-        if self.state == GearState.DOWN_LOCKED:
+        if self.state == GearState.DOWN_LOCKED:                     # Confirms the gear is in a valid state
             self.state = GearState.TRANSITIONING_UP
             self.log("Gear Retracting")
-            time.sleep(1)
-            self.state = GearState.UP_LOCKED
-            self.log("Gear Moved Up")
+            HydraulicsAngle, timePeriod = HydraulicActuators.retracting(Hydraulics, 1200) # Calls the hydraulics to move
+            if HydraulicsAngle == 0:                                # If the gear is retracted enough
+                self.state = GearState.DOWN_LOCKED
+                self.log("Gear Retracted")
+            else:                                                   # If the gear is not retracted enough
+                self.log("Insufficient Gear Retraction angle")
         else:
-            self.log("Command Rejected, Invalid State")
+            self.log("Command Rejected")
 
-Hydraulics = HydraulicActuators
-Hydraulics.temperature = 30
-HydraulicActuators.refreshEfficiency(Hydraulics, 100, 3000)
+debug = True                                                        # Sets the debug of the script t/f
+Hydraulics = HydraulicActuators                                     # Creates an object of the class Hydraulics
+Hydraulics.temperature = 30 
+HydraulicActuators.refreshEfficiency(Hydraulics, 80, 500)           # Calculates the hydraulic efficiency
+
 controller = LandingGearController()
 controller.command_gear_down()
 controller.command_gear_up()
-#angle, time = HydraulicActuators.extending(Hydraulics, 1200)
-#print(time)
